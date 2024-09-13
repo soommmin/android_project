df99bf9f649e919733322a3eba40e64350ef9ca4
{soommmin} <tnakszla@gmail.com>
2024-09-13 14:31:03 +0900

    first commit

---
+++ app/src/main/java/android/part1/android_pro_04_picture/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.part1.android_pro_04_picture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
